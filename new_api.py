95ca3b62ff715395cfc3941e35b60e26aac263af
latidoenlinea <latidoenlinea@gmail.com>
2024-11-11 12:01:32 -0600

    Update new_api.py

--- new_api.py
+++ new_api.py
@@ -81,4 +81,4 @@ def process_frame():
         return jsonify({'error': 'Error en el procesamiento de la imagen'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
