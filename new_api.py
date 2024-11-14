dbbb096f8711a8a27625c277c5eb3286d8ed161b
latidoenlinea <latidoenlinea@gmail.com>
2024-11-14 13:15:23 -0600

    Update new_api.py

--- new_api.py
+++ new_api.py
@@ -67,7 +67,7 @@ def process_frame():
         faces = face_cascade.detectMultiScale(
             gray,
             scaleFactor=1.05,  # Aumenta la precisión (menos falsos positivos)
-            minNeighbors=7,   # Ajuste de vecinos mínimos
+            minNeighbors=6,   # Ajuste de vecinos mínimos
             minSize=(50, 50), # Tamaño mínimo del rostro
             maxSize=(500, 500) # Tamaño máximo del rostro
         )
